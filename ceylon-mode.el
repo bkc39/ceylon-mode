68535f1f99fbe950573959a5672b8e8266d4a8dc
bkc39 <bkc39@cornell.edu>
2014-04-27 13:32:37 -0400

    Add the syntax highlighter.

--- ceylon-mode.el
+++ ceylon-mode.el
@@ -10,7 +10,7 @@
 (add-to-list 'auto-mode-alist '("\\.ceylon" . ceylon-mode))
 
 ;; Control flow keywords
-(setq ceylon-control-flow-keywords
+(defvar ceylon-control-flow-keywords
       '("assert"
         "break"
         "case"
@@ -27,14 +27,14 @@
         "while"))
 
 ;; Keywords for the Object Oriented paradigm
-(setq ceylon-class-keywords
+(defvar ceylon-class-keywords
       '("class"
         "extends"
         "interface"
         "module"
         "object"))
 
-(setq ceylon-special-keywords
+(defvar ceylon-special-keywords
       '("continue"
         "return"
         "super"
@@ -43,28 +43,101 @@
         "value"
         "void"))
 
-(setq ceylon-type-keywords
+(defvar ceylon-type-keywords
       '("alias"
         "assign"
         "dynamic"
         "value"))
 
-(setq ceylon-operator-keywords
+(defvar ceylon-operator-keywords
       '("exists"
         "in"
         "is"
         "nonempty"
         "of"))
 
-(setq ceylon-constraint-keywords
+(defvar ceylon-constraint-keywords
       '("satisfies"
         "given"
         "out"))
 
-(setq ceylon-package-keywords
+(defvar ceylon-package-keywords
       '("import"
         "package"))
 
-(setq ceylon-unused-keywords
+(defvar ceylon-unused-keywords
       '("assembly"
         "abstracts"))
+
+(defvar ceylon-doc-keywords
+  '("doc" "by" "see" "throws" "tagged"))
+
+
+(defvar ceylon-control-flow-keywords-regexp
+  (regexp-opt ceylon-control-flow-keywords 'words))
+
+(defvar ceylon-class-keywords-regexp
+  (regexp-opt ceylon-class-keywords 'words))
+
+(defvar ceylon-special-keywords-regexp
+  (regexp-opt ceylon-special-keywords 'words))
+
+(defvar ceylon-type-keywords-regexp
+  (regexp-opt ceylon-type-keywords 'words))
+
+(defvar ceylon-operator-keywords-regexp
+  (regexp-opt ceylon-operator-keywords 'words))
+
+(defvar ceylon-constraint-keywords-regexp
+  (regexp-opt ceylon-constraint-keywords 'words))
+
+(defvar ceylon-package-keywords-regexp
+  (regexp-opt ceylon-package-keywords 'words))
+
+(defvar ceylon-unused-keywords-regexp
+  (regexp-opt ceylon-unused-keywords 'words))
+
+(defvar ceylon-doc-keywords-regexp
+  (regexp-opt ceylon-doc-keywords 'words))
+
+;; Clear the memory allocated to the lists now that the regexp's are
+;; created
+(setq ceylon-control-flow-keywords nil)
+(setq ceylon-class-keywords nil)
+(setq ceylon-special-keywords nil)
+(setq ceylon-type-keywords nil)
+(setq ceylon-operator-keywords nil)
+(setq ceylon-constraint-keywords nil)
+(setq ceylon-package-keywords nil)
+(setq ceylon-unused-keywords nil)
+(setq ceylon-doc-keywords nil)
+
+;; Creates the list for font-lock. Each class of keywords is given a
+;; particular face.
+(defvar ceylon-font-lock-keywords
+  `((,ceylon-doc-keywords-regexp . font-lock-keyword-face)
+    (,ceylon-control-flow-keywords-regexp . font-lock-keyword-face)
+    (,ceylon-class-keywords-regexp . font-lock-keyword-face)
+    (,ceylon-special-keywords-regexp . font-lock-keyword-face)
+    (,ceylon-type-keywords-regexp . font-lock-type-face)
+    (,ceylon-operator-keywords-regexp . font-lock-builtin-face)
+    (,ceylon-constraint-keywords-regexp . font-lock-keyword-face)
+    (,ceylon-package-keywords-regexp . font-lock-keyword-face)
+    (,ceylon-unused-keywords-regexp . font-lock-warning-face)))
+
+(define-derived-mode ceylon-mode fundamental-mode
+  "Ceylon mode"
+  "Major mode for editing Ceylon files."
+  (setq font-lock-defaults '((ceylon-font-lock-keywords)))
+  ;; clear memory
+  (setq ceylon-control-flow-regexp nil)
+  (setq ceylon-class-regexp nil)
+  (setq ceylon-special-regexp nil)
+  (setq ceylon-type-regexp nil)
+  (setq ceylon-operator-regexp nil)
+  (setq ceylon-constraint-regexp nil)
+  (setq ceylon-package-regexp nil)
+  (setq ceylon-unused-regexp nil)
+  (setq ceylon-doc-regexp nil))
+
+(provide 'ceylon-mode)
